4970afca252b08a94d1552ceb6be04373b2d2928
OsmarAlvarado <osmar.alvarado.dev@gmail.com>
2022-11-16 20:17:45 +0100

    exercices

---
+++ main.js
@@ -0,0 +1,18 @@
+var summation = function (num) {
+    let sum;
+    return sum = num * (num + 1) / 2
+
+}
+
+console.log(summation(4));
+
+//-- otra forma del de arriba
+// var summation = function (num) {
+//     let result = 0;
+//     for (var i = 1; i <= num; i++) {
+//         result += i;
+//     }
+
+//     return result;
+// }
+
